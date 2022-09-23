fd673555a5ffd8ccb05b509392fb537067616ba8
Shakil Ahmed <shakilahmmeed@gmail.com>
2022-09-24 01:29:59 +0600

    landing page added

---
+++ landing/src/components/logo.js
@@ -0,0 +1,22 @@
+/** @jsx jsx */
+import { jsx, Image } from "theme-ui";
+import { Link } from "components/link";
+
+export default function Logo({ image }) {
+	return (
+		<Link
+			path="/"
+			sx={{
+				variant: "links.logo",
+				display: "flex",
+				alignItems: "center",
+			}}
+		>
+			<Image
+				src={image}
+				alt="Testfuel - Your QA made simple"
+				style={{ height: 32 }}
+			/>
+		</Link>
+	);
+}
---
+++ landing/src/pages/_document.js
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import favicon from "assets/favicon.png";
+
+class CustomDocument extends Document {
+	static async getInitialProps(ctx) {
+		const initialProps = await Document.getInitialProps(ctx);
+		return { ...initialProps };
+	}
+
+	render() {
+		return (
+			<Html lang="en-US">
+				<Head>
+					<link rel="shortcut icon" href={favicon} />
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
+
+export default CustomDocument;
