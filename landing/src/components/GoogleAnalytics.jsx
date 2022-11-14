0e29dd8e08eca574cff5bd5635a0a5d0401febd5
Shakil Ahmed <shakilahmmeed@gmail.com>
2022-11-14 08:16:30 +0600

    feat: ga setup on landing page

--- landing/src/components/GoogleAnalytics.jsx
+++ landing/src/components/GoogleAnalytics.jsx
@@ -1,22 +1,19 @@
-import React from 'react'
-import Script from 'next/script'
+import React from "react";
+import { Helmet } from "react-helmet";
 
 export default function GoogleAnalytics() {
-  return <div>
-    <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}/>
-    <Script
-      id='google-analytics'
-      strategy="afterInteractive"
-      dangerouslySetInnerHTML={{
-        __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', {
-            page_path: window.location.pathname,
-          });
-        `,
-        }}
-    />
-  </div>
+	return (
+		<Helmet>
+			<script
+				async
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+			></script>
+			<script>
+				{`window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+					gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');`}
+			</script>
+		</Helmet>
+	);
 }
