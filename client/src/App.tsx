57e149ba25136316875a24ccc5903b80d5892f42
Shakil Ahmed <shakilahmmeed@gmail.com>
2022-11-15 07:13:50 +0600

    fix: react-ga updated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,10 @@
 import React, { useEffect } from "react";
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
 import { useDispatch } from "react-redux";
 import { loadUser } from "store/auth";
 
-ReactGA.initialize(process.env.REACT_APP_GA_PROPERTY_ID as any);
-ReactGA.set({ page: window.location.pathname });
-ReactGA.pageview(window.location.pathname + window.location.search);
+ReactGA.initialize(process.env.REACT_APP_GA_PROPERTY_ID as string);
+ReactGA.send("pageview");
 
 function App({ children }: { children: React.ReactElement }) {
 	const dispatch = useDispatch();
