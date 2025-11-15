1c2803558596a9f119d7e7b1f835de385fee05bf
Shakil Ahmed <shakilahmmeed@gmail.com>
2025-11-15 20:37:47 +0100

    feat: add custom login cover image

--- client/src/components/Layout/LoginLayout.tsx
+++ client/src/components/Layout/LoginLayout.tsx
@@ -10,10 +10,7 @@ export default function LoginLayout({ children, title }: LoginLayoutProps) {
 	return (
 		<section className="flex flex-col md:flex-row h-screen items-center">
 			<div className="bg-indigo-600 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
-				<img
-					src="https://source.unsplash.com/random?tech,night,rocket"
-					className="w-full h-full object-cover"
-				/>
+				<img src="/login-cover.avif" className="w-full h-full object-cover" />
 			</div>
 			<div className="bg-white w-full md:max-w-md lg:max-w-full md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12 flex items-center justify-center">
 				<div className="w-full h-100">
